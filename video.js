b1723aaab3ba156edff13a23d48689509a6903b9
Michael Saunby <mike@saunby.net>
2014-06-07 12:31:44 +0100

    added zoom slider

--- video.js
+++ video.js
@@ -6,7 +6,9 @@ var camera, scene, renderer;
 var group;
 //var mouseX = 0, mouseY = 0;
 var longitudeA = 0, latitudeA = 0;
+var zoom = 0;
 var texture, texture2;
+var cameraZ = 2000;
 
 init();
 animate();
@@ -16,7 +18,7 @@ function init() {
     container = document.getElementById( 'globediv' );
     
     camera = new THREE.PerspectiveCamera( 60, 1, 1, 10000 );
-    camera.position.z = 3000;
+    camera.position.z = cameraZ;
 
     scene = new THREE.Scene();
     
@@ -31,7 +33,7 @@ function init() {
     scene.add( group );
     
        
-    var geometry = new THREE.SphereGeometry( 1000, 30, 30 );
+    var geometry = new THREE.SphereGeometry( 800, 40, 40 );
     //var geometry2 = new THREE.SphereGeometry( 1010, 30, 30 );
     texture = new THREE.Texture( image );
     //texture.offset.set(0.5,0);
@@ -63,6 +65,7 @@ function init() {
 
     jQuery('#xsliderA').on('slide', function(event, ui){ longitudeA = ui.value;} );
     jQuery('#ysliderA').on('slide', function(event, ui){ latitudeA = ui.value;} );
+    jQuery('#zslider').on('slide', function(event, ui){ zoom = ui.value;} );
 }
 
 function newGlobe(image) {
@@ -105,6 +108,7 @@ function render() {
 
     group.rotation.x = 3.14/180.0 * latitudeA;
     group.rotation.y = 3.14/180.0 * (longitudeA - 90.0);
+    camera.position.z = cameraZ - (zoom * 10);
 
     if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
 
